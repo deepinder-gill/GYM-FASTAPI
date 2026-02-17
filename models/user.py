ee7a76d592f10a3b08957d5db485a34b395748a5
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-18 04:14:21 +0530

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 class UserCreate(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field( min_length=8, max_length=64)
 
 class UserOut(BaseModel):
     email: EmailStr
