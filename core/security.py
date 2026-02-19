c68167cd82d1175b9a27ad4885a6e6b057467a39
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-20 03:45:12 +0530

    Update security.py

--- core/security.py
+++ core/security.py
@@ -1,5 +1,11 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
 from typing_extensions import deprecated
+
+SECRET_KEY = "supersecretkeychangeinproduction"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -8,3 +14,7 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=15)
