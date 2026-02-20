c42b589b40e94d0ce59b5ff0219b6eed6b2ea1cb
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-21 03:39:12 +0530

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from app.database import Base
 
 class User(Base):
@@ -7,3 +7,10 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+class RefreshToken(Base):
+    __tablename__ = "refresh_tokens"
+
+    id = Column(Integer, primary_key=True, index=True)
+    token = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
