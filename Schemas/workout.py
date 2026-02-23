6cfb2bd200ee93c004c3ddace1fdde08e910a0a3
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-24 02:28:51 +0530

    Update workout.py

--- Schemas/workout.py
+++ Schemas/workout.py
@@ -1,6 +1,36 @@
 from pydantic import BaseModel
 from typing import Optional, List
+from datetime import datetime
 
-class WorkoutLog(BaseModel):
+class WorkoutLogCreate(BaseModel):
     note : Optional[str] = None
-    exercises : List
+    exercises : List[WorkoutExerciseCreate]
+
+class WorkoutLogOut(BaseModel):
+    id : int
+    user_id : int
+    created_at : datetime
+    note : Optional[str] = None
+    exercises : List[WorkoutExerciseOut]
+
+    class Config:
+        orm_mode = True
+
+class WorkoutExerciseCreate(BaseModel):
+    exercise_name : str
+    sets : int
+    reps : int
+    weight : float
+
+class WorkoutExerciseOut(BaseModel):
+    id : int
+    exercise_name : str
+    sets : int
+    reps : int
+    weight : float
+
+    class Config:
+        orm_mode =True
+
+
+
