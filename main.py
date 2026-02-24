5753ecfb42172367a10258a9413bf54be68d6d4b
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-25 00:06:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from fastapi import FastAPI
 from app.routes.user_routes import router
 from app.database import engine, Base
 from app.models.models import User
-from app.routes.workout import workoutrouter
+from app.routes.workout_route import workoutrouter
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
-app.include_router(router, workoutrouter)
+app.include_router(router)
+
+app.include_router(workoutrouter)
+
+
 
 
 
