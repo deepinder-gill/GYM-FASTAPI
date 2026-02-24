cb8af477a991e07f57345fedbea752864c8a9eef
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-25 03:49:52 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./gym.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:wGPZUCsRuEmYArLLkrBUbswRlWAfWgaX@yamanote.proxy.rlwy.net:49086/railway")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
